213b9de828e089bed9beaa0ff5356c012a446871
Liz Wigglesworth <lizwigg6@gmail.com>
2018-04-24 13:42:53 -0500

    Read angle from controller

--- Pool/Pool/Board.cs
+++ Pool/Pool/Board.cs
@@ -11,7 +11,6 @@ namespace Pool
     {
         Player[] players;
         List<Ball> balls;
-        Physics physics;
         GUI gui;
         List<Zone> zones;
 
@@ -20,8 +19,30 @@ namespace Pool
         public Board(int numPlayers)
         {
             players = new Player[numPlayers];
+            // create all players
+            for (int i = 0; i < players.Length; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        players[i] = new Player(Color.Red, PlayerIndex.One);
+                        break;
+                    case 1:
+                        players[i] = new Player(Color.Blue, PlayerIndex.Two);
+                        break;
+                    case 2:
+                        players[i] = new Player(Color.Green, PlayerIndex.Three);
+                        break;
+                    case 3:
+                        players[i] = new Player(Color.Yellow, PlayerIndex.Four);
+                        break;
+                    default:
+                        Console.WriteLine("Error - there should be 1-4 players");
+                        break;
+                }
+            }
+
             balls = new List<Ball>(2);
-            physics = new Physics();
             friction = 0;
 
             gui = new GUI();
@@ -30,12 +51,18 @@ namespace Pool
 
         public void Update(GameTime gameTime)
         {
-
+            foreach (Player p in players)
+            {
+                p.Update(gameTime);
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-
+            foreach (Player p in players)
+            {
+                p.Draw(spriteBatch);
+            }
         }
     }
 }
